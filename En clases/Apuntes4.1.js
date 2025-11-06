1c22a407b959e51068375ffd931e1ccbda0bfb25
Isai Cardenas <isaicardenas846@gmail.com>
2025-11-06 12:43:50 -0600

    apuntes

---
+++ En clases/Apuntes4.1.js
@@ -0,0 +1,12 @@
+
+//Arreglos
+
+function DatosArreglos(){
+    let Datos=[3,7,9,3,2,6]
+    console.log(Datos.length)
+}
+DatosArreglos()
+/*  
+0 1 2 3 4 5 index
+3 7 9 3 2 6 valor
+*/
---
+++ En clases/Apuntes4.2.js
@@ -0,0 +1,21 @@
+function DatosArreglos(){
+    let Datos=[3,7,9,3,2,6]
+    let suma=0
+    for(i=0;i<Datos.length;i++){
+        if(Datos[i]!=0){
+            suma+=Datos[i]
+        }
+    } console.log(`la suma de numeros impares es ${suma}`)
+} DatosArreglos()
+
+/* 
+ESC
+_for.. of
+_while
+_do_while
+_for..in
+_for coch
+_map
+_reduce
+_filter
+*/ 
